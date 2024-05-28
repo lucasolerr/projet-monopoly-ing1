dd292dac88dcd2e384448714ef5b463caa014493
Luca Soler <luca.soler@edu.ece.fr>
2024-05-28 13:21:58 +0200

    delete old files

--- Pages/MenuPrincipal/menuPrincipal.h
+++
@@ -1,9 +0,0 @@
-#ifndef ING1_PROJET_2021_22_DOFUS_ARENA_EQUIPE_X_1_MENUPRINCIPAL_H
-#define ING1_PROJET_2021_22_DOFUS_ARENA_EQUIPE_X_1_MENUPRINCIPAL_H
-
-#include "../../Structures/structures.h"
-#include "../PageAide/pageAide.h"
-
-void boucleMenuPrincipal(Dofus* dofus);
-
-#endif
